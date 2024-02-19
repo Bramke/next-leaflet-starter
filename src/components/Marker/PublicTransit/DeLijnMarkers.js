77b3d2be228e46211b6f00a12fcaccc4f8c9141f
Bram Krick <bram.krick@optioryx.com>
2024-02-19 23:44:49 +0100

    🧑‍🎨 Restyling delijnmarker

--- src/components/Marker/PublicTransit/DeLijnMarkers.js
+++ src/components/Marker/PublicTransit/DeLijnMarkers.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import useSWR from 'swr'
-import { Avatar, Entity, EntityField, Menu, MenuButton, MenuItem, MenuWrapper, fs, Button, Text, Code, Spacer } from 'nextjs-components';
+import { Avatar, Entity, EntityField, Menu, MenuButton, MenuItem, MenuWrapper, fs, Button, Text, Code, Spacer, Tooltip } from 'nextjs-components';
 import { IconDeviceMobileMessage } from '@tabler/icons-react';
+import Link from 'next/link';
 
 export default function DeLijnMarkers({ Marker, Popup }) {
   const { data: markerData } = useSWR('/api/publicTransit/delijn', axios);
@@ -21,16 +22,15 @@ export default function DeLijnMarkers({ Marker, Popup }) {
               <fs.Fieldset>
                 <fs.Content>
                   <fs.Title>Halte {halte.omschrijving}</fs.Title>
-                  <fs.Subtitle>{halte.haltenummer}</fs.Subtitle>
-                  <iframe src={`https://www.delijn.be/realtime/${halte.haltenummer}/15`} width="270" height="400" />
+                  <Tooltip text="Open halte info" position="right"><fs.Subtitle><Link href={(`https://www.delijn.be/en/haltes/${halte.haltenummer}/`)} src={`https://www.delijn.be/realtime/${halte.haltenummer}/15`}>{halte.haltenummer}</Link></fs.Subtitle></Tooltip>
+                  <iframe onClick={() => window.open(`https://www.delijn.be/en/haltes/${halte.haltenummer}/`)} src={`https://www.delijn.be/realtime/${halte.haltenummer}/15`} width="270" height="400" />
                 </fs.Content>
                 <fs.Footer>
                   <fs.Footer.Status>
                     <Button size="small" prefix={<IconDeviceMobileMessage />} style={{marginRight: '10px'}} onClick={() => window.open(`sms:4884&body=DL`)}>1U Ticket</Button>
-                    <Button size="small" prefix={<IconDeviceMobileMessage />} onClick={() => window.open(`sms:4884&body=DLD`)}>DAG Ticket</Button>
                   </fs.Footer.Status>
                   <fs.Footer.Actions>
-                    <Button size="small" onClick={() => window.open(`https://www.delijn.be/en/haltes/${halte.haltenummer}/`)}>Halte Info</Button>
+                    <Button size="small" prefix={<IconDeviceMobileMessage />} onClick={() => window.open(`sms:4884&body=DLD`)}>DAG Ticket</Button>
                   </fs.Footer.Actions>
                 </fs.Footer>
               </fs.Fieldset>
